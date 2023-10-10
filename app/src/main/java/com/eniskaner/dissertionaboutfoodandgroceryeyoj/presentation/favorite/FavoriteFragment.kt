85afff07e10b7f7966911ce6d331632662680de6
Enis Kaner <enis.d.kaner@gmail.com>
2023-10-10 16:19:04 +0300

    Add: dependency injection components

--- app/src/main/java/com/eniskaner/dissertionaboutfoodandgroceryeyoj/presentation/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/eniskaner/dissertionaboutfoodandgroceryeyoj/presentation/favorite/FavoriteFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.eniskaner.dissertionaboutfoodandgroceryeyoj.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoriteFragment : Fragment() {
 
     override fun onCreateView(
